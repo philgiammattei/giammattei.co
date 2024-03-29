7b117b34d35833d70ec42a82ec0805861bbe80f6
Phil Giammattei <philgiammattei@gmail.com>
2024-03-29 10:12:16 -0400

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Phil Giammattei';
+export const SITE_DESCRIPTION = 'A blog and portfolio for Phil Giammattei, a web site maker and software engineer based in Pittsburgh, PA.';
