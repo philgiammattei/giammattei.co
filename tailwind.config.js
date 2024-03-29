88d18cb03b8af0c0e083809e193ce13481c3ac05
Phil Giammattei <philgiammattei@gmail.com>
2024-03-29 13:33:33 -0400

    header styles, tailwind, scss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ['./src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+}
