7372c7c037d55d93e61deda965093afa7a92dd95
Phil Giammattei <philgiammattei@gmail.com>
2024-04-03 13:02:50 -0400

    more projects and blog refinement

--- src/components/ConditionalBlogList.jsx
+++ src/components/ConditionalBlogList.jsx
@@ -1,41 +1,54 @@
-import FormattedDate from './FormattedDate';
-import { useState } from 'preact/hooks';
+import FormattedDate from "./FormattedDate";
+import { useState } from "preact/hooks";
 
-const ConditionalBlogList = ({allPosts, filteredPosts}) => {
+const ConditionalBlogList = ({ allPosts, filteredPosts }) => {
   const [showProjects, setShowProjects] = useState(false);
-  const [posts, setPosts] = useState(showProjects ? allPosts :filteredPosts);
-
+  const [posts, setPosts] = useState(showProjects ? allPosts : filteredPosts);
 
   const handleClick = () => {
-    console.log('click');
+    console.log("click");
     setShowProjects((prevShowProjects) => {
       const newShowProjects = !prevShowProjects;
-      setPosts(newShowProjects ? allPosts: filteredPosts);
+      setPosts(newShowProjects ? allPosts : filteredPosts);
       return newShowProjects;
-    })
-  }
+    });
+  };
   return (
-  <section>
-      <h2>Blog</h2>
-    <input type="checkbox" id="show-projects" checked={showProjects} onClick={(handleClick)}/>
-    <label htmlFor="show-projects">Show Project Posts</label>
-    <ul className="blog-list">
-      {
-        posts.map((post) => (
+    <section>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="mb-0">Blog</h2>
+        <div class="checkbox">
+          <input
+            type="checkbox"
+            id="show-projects"
+            checked={showProjects}
+            onClick={handleClick}
+            className="mr-2"
+          />
+          <label htmlFor="show-projects">Show Project Posts</label>
+        </div>
+      </div>
+
+      <ul className="blog-list">
+        {posts.map((post) => (
           <li>
             <a href={`/blog/${post.slug}/`}>
-              <img width={720} height={360} src={post.data.heroImage} alt="" />
-              <h4 className="title">{post.data.title}</h4>
+              <img src={post.data.heroImage} alt="" />
+            </a>
+            <div className="flex flex-col items-start">
               <p className="date">
                 <FormattedDate date={post.data.pubDate} />
               </p>
-            </a>
+              <a href={`/blog/${post.slug}/`}>
+                <h4 className="title">{post.data.title}</h4>
+              </a>
+              <p>{post.data.description}</p>
+            </div>
           </li>
-        ))
-      }
-    </ul>
-  </section>
-  )
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default ConditionalBlogList;
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,9 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
@@ -11,6 +14,8 @@ export async function GET(context) {
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
+			content: sanitizeHtml(parser.render(post.body)),
 		})),
 	});
 }
+
