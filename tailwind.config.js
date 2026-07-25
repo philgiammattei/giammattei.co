d0d5144e3cc39ad3d199571e3a38a80707e5060e
Phil Giammattei <philgiammattei@gmail.com>
2026-07-25 17:42:20 -0400

    dependency updates, tiger pajamas

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  purge: ['./src/**/*.{astro,js,jsx,ts,tsx,vue}'],
-}
